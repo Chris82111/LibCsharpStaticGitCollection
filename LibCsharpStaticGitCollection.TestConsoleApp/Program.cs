5e229ecd5eb503bb7b65268123986fa9068daccc
Chris82111 <75613723+Chris82111@users.noreply.github.com>
2025-11-08 20:06:55 +0100

    A project for testing the library has been added

---
+++ LibCsharpStaticGitCollection.TestConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace LibCsharpStaticGitCollection.TestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Version: {GitVersion().Result}");
+        }
+
+        public static async Task<string> GitVersion()
+        {
+            var psi = new ProcessStartInfo
+            {
+                //FileName = "git",
+                FileName = "./Lib/MinGit-2.51.2-64-bit/cmd/git.exe",
+                Arguments = "-v",
+                WorkingDirectory = ".",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            psi.Environment["GIT_ASKPASS"] = "echo";
+
+            var process = new Process { StartInfo = psi };
+
+            if (null == process)
+            {
+                return string.Empty;
+            }
+
+            process.Start();
+
+            //TODO: Debug
+            string output = await process.StandardOutput.ReadToEndAsync();
+
+            await process.WaitForExitAsync();
+
+            return output;            
+        }
+    }
+}
